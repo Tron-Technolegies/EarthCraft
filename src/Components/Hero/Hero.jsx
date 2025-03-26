3120d4fa947027edc476aed5ed3ba6f63ce62033
aswinbinaj <133196602+aswinbinaj@users.noreply.github.com>
2025-03-26 16:27:11 +0530

    updated video

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 // import homepagevideo from "../../assets/homepagevideo.mp4";
 import caletalhomepagevideo from "../../assets/caletalhomepagevideo.mp4";
+import caletal_projects from "../../assets/caletal_projects.mp4";
 import "../Hero/Hero.css";
 const Hero = () => {
   return (
     <div className="herosection">
-      <video src={caletalhomepagevideo} autoPlay loop muted playsInline ></video>
+      <video src={caletal_projects} autoPlay loop muted playsInline ></video>
     </div>
   );
 };
